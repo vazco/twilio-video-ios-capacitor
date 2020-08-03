ee710165497da8dceb48e4da1d0f0cc883e4cf72
agodin3z <pistilloagodinez@gmail.com>
2020-08-03 17:59:12 -0600

    refactor(ios/Plugin): rename plugin name and plugin method

--- ios/Plugin/Plugin/Plugin.m
+++ ios/Plugin/Plugin/Plugin.m
@@ -4,6 +4,6 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 
-CAP_PLUGIN(TwilioIosCapacitor, "TwilioIosCapacitor",
-          CAP_PLUGIN_METHOD(joinTwilioRoom, CAPPluginReturnPromise);
+CAP_PLUGIN(TwilioVideoIosCapacitor, "TwilioVideoIos",
+          CAP_PLUGIN_METHOD(joinRoom, CAPPluginReturnPromise);
 )
